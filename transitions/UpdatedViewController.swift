14cc5466e503fce0918e8d8f2c8224e49fb2ba79
kmart22 <kmar7070@740s-pl-111.local>
2016-10-03 09:02:19 -0600

    added images to view

--- transitions/UpdatedViewController.swift
+++ transitions/UpdatedViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class UpdatedViewController : UIViewController
 {
+    @IBOutlet private weak var topImage: UIImageView!
+    @IBOutlet private weak var bottomImage: UIImageView!
+
     override func viewDidLoad()
     {
         //Load the parent version of a view
         super.viewDidLoad()
         //Load this pages components
+        bottomImage.image = UIImage(named: "glitchy")
     }
 }
